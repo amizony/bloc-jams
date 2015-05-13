786c84eb539f9842259b9ad6e3997e4b21962135
amizony <amizony@gmail.com>
2015-05-13 13:27:12 +0000

    Added profile and subscription page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -255,9 +255,10 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
- require("./landing");
- require("./collection");
- require("./album");
+require("./landing");
+require("./collection");
+require("./album");
+require("./profile");
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -346,6 +347,32 @@ if (document.URL.match(/\/collection.html/)) {
  
     $('.selling-points .point').hover(onHoverAction, offHoverAction);
   });
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+// holds the name of our tab button container for selection later in the function
+var tabsContainer = ".user-profile-tabs-container"
+var selectTabHandler = function(event) {
+};
+var tabsContainer = ".user-profile-tabs-container"
+var selectTabHandler = function(event) {
+	$tab = $(this);
+	$(tabsContainer + " li").removeClass('active');
+	$tab.parent().addClass('active');
+	selectedTabName = $tab.attr('href');
+	console.log(selectedTabName);
+	$(".tab-pane").addClass('hidden');
+	$(selectedTabName).removeClass('hidden');
+	event.preventDefault();
+};
+
+if (document.URL.match(/\/profile.html/)) {
+	$(document).ready(function() {
+		var $tabs = $(tabsContainer + " a");
+		$tabs.click(selectTabHandler);
+		$tabs[0].click();
+	});
+}
 });
 
 ;
