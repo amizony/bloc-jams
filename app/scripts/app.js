e1ad247ddcbaf4cc3b1fba9c2065360d3935643d
amizony <amizony@gmail.com>
2015-05-15 15:29:06 +0000

    Convert landing page using Angularjs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,36 @@
-require("./landing");
-require("./collection");
-require("./album");
-require("./profile");
+//require("./landing");
+//require("./collection");
+//require("./album");
+//require("./profile");
+
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+
+	function shuffle(o){ //v1.0
+		for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+			return o;
+	};
+
+	$scope.title = "Bloc Jams";
+	$scope.subText = "Turn the music up!";
+
+	$scope.albumURLs = [
+		'/images/album-placeholders/album-1.jpg',
+		'/images/album-placeholders/album-2.jpg',
+		'/images/album-placeholders/album-3.jpg',
+		'/images/album-placeholders/album-4.jpg',
+		'/images/album-placeholders/album-5.jpg',
+		'/images/album-placeholders/album-6.jpg',
+		'/images/album-placeholders/album-7.jpg',
+		'/images/album-placeholders/album-8.jpg',
+		'/images/album-placeholders/album-9.jpg',
+	];
+
+
+	$scope.titleClicked = function() {
+		shuffle($scope.albumURLs);
+	};
+
+	$scope.subTextClicked = function() {
+		$scope.subText += '!';
+	};
+}]);
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -255,10 +255,42 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing");
-require("./collection");
-require("./album");
-require("./profile");
+//require("./landing");
+//require("./collection");
+//require("./album");
+//require("./profile");
+
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+
+	function shuffle(o){ //v1.0
+		for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+			return o;
+	};
+
+	$scope.title = "Bloc Jams";
+	$scope.subText = "Turn the music up!";
+
+	$scope.albumURLs = [
+		'/images/album-placeholders/album-1.jpg',
+		'/images/album-placeholders/album-2.jpg',
+		'/images/album-placeholders/album-3.jpg',
+		'/images/album-placeholders/album-4.jpg',
+		'/images/album-placeholders/album-5.jpg',
+		'/images/album-placeholders/album-6.jpg',
+		'/images/album-placeholders/album-7.jpg',
+		'/images/album-placeholders/album-8.jpg',
+		'/images/album-placeholders/album-9.jpg',
+	];
+
+
+	$scope.titleClicked = function() {
+		shuffle($scope.albumURLs);
+	};
+
+	$scope.subTextClicked = function() {
+		$scope.subText += '!';
+	};
+}]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
