db2c86959680d7177e9561d6dc3187c78a23a4e5
amizony <amizony@gmail.com>
2015-05-15 16:36:50 +0000

    Converted collection view to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -262,6 +262,10 @@ if (document.URL.match(/\/album.html/)) {
 
 blocJams = angular.module('BlocJams', ['ui.router']);
 
+
+// ------------------------------
+// Navigation
+
 blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
    $locationProvider.html5Mode(true);
  
@@ -276,8 +280,70 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
      //controller: 'Landing.controller',
      templateUrl: '/templates/song.html'
    });
+
+   $stateProvider.state('collection', {
+     url: '/collection',
+     controller: 'Collection.controller',
+     templateUrl: '/templates/collection.html'
+   });
  }]);
 
+
+// ------------------------------
+// Albums
+
+var albumPicasso = {
+   name: 'The Colors',
+   artist: 'Pablo Picasso',
+   label: 'Cubism',
+   year: '1881',
+   albumArtUrl: '/images/album-placeholder.png',
+ 
+   songs: [
+       { name: 'Blue', length: '4:26' },
+       { name: 'Green', length: '3:14' },
+       { name: 'Red', length: '5:01' },
+       { name: 'Pink', length: '3:21'},
+       { name: 'Magenta', length: '2:15'}
+     ]
+ };
+var albumMarconi = {
+  name: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: '/images/album-placeholder.png',
+  songs: [
+  { name: 'Hello, Operator?', length: '1:01' },
+  { name: 'Ring, ring, ring', length: '5:01' },
+  { name: 'Fits in your pocket', length: '3:21'},
+  { name: 'Can you hear me now?', length: '3:14' },
+  { name: 'Wrong phone number', length: '2:15'}
+  ]
+};
+var albumTSFH = {
+	name: 'Illusions',
+	artist: 'Thomas J. Bergersen',
+	label: 'Orchestral',
+	year: '2011',
+	albumArtUrl: '/images/album-placeholder.png',
+	songs: [
+  { name: 'Aura', length: '7:43' },
+  { name: 'Starvation', length: '4:27' },
+  { name: 'Dreammaker', length: '4:18'},
+  { name: 'Hurt', length: '1:43' },
+  { name: 'Ocean Princess', length: '2:53'},
+  { name: 'Gift of Life', length: '3:22'},
+  { name: 'Rada', length: '4:23'},
+  { name: 'A Place In Heaven', length: '4:16'},
+  { name: 'Merchant Prince', length: '2:226'},
+  { name: 'Promise', length: '4:59	'},
+  ]
+}
+
+
+// ------------------------------
+// Controllers
 
 blocJams.controller('Landing.controller', ['$scope', function($scope) {
 
@@ -309,6 +375,21 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
 	$scope.subTextClicked = function() {
 		$scope.subText += '!';
 	};
+}]);
+
+
+blocJams.controller('Collection.controller', ['$scope', function($scope) {
+   $scope.albums = [];
+   for (var i = 0; i < 33; i++) {
+   	var rand = Math.random();
+   	if (rand < 0.34) {
+   		$scope.albums.push(angular.copy(albumPicasso));
+   	} else if (rand < 0.67) {
+   		$scope.albums.push(angular.copy(albumMarconi));
+   	} else {
+   		$scope.albums.push(angular.copy(albumTSFH));
+   	}
+   }
 }]);
 });
 
